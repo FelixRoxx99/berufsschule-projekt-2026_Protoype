489f965fcc9c3992c418a3eb36cd3ba40e5cb696
FelixRoxx99 <115409544+FelixRoxx99@users.noreply.github.com>
2026-08-08 14:57:39 +0000

    new

--- backend/settings.py
+++ backend/settings.py
@@ -1,8 +1,9 @@
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
+INSTALLED_APPS += ['rest_framework']
