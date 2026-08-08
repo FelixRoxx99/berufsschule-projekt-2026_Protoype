b9499169ad27094760abf79fedcce6f63e52afeb
FelixRoxx99 <115409544+FelixRoxx99@users.noreply.github.com>
2026-08-08 15:28:15 +0000

    fixes

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let msg = "Lade…";
+
+    fetch("https://animated-pancake-xxxxxx-8000.app.github.dev/")
+        .then(r => r.json())
+        .then(data => {
+            msg = data.msg;
+        })
+        .catch(err => {
+            msg = "Fehler beim Laden der API";
+            console.error(err);
+        });
+</script>
+
+<h1>{msg}</h1>
+
